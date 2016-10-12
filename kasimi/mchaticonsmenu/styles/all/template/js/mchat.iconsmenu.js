a802644fd18c53f0b31af53e3d0bac904221f45c
kasimi <mail@kasimi.net>
2016-10-12 21:55:03 +0200

    Fixed displaying menu for new and edited messages

--- kasimi/mchaticonsmenu/styles/all/template/js/mchat.iconsmenu.js
+++ kasimi/mchaticonsmenu/styles/all/template/js/mchat.iconsmenu.js
@@ -7,10 +7,27 @@
  */
 
 jQuery(function ($) {
+
 	"use strict";
+
 	var $container = $('.dropdown-container-mchat-message-icons');
 	$container.on('click', 'a', function() {
 		$container.removeClass('dropdown-visible');
 		$(this).closest('.dropdown').toggle();
+	});
+
+	var registerDropdown = function($message) {
+		var $trigger = $message.find('.dropdown-trigger:first');
+		var $contents = $message.find('.dropdown');
+		phpbb.registerDropdown($trigger, $contents, {});
+	};
+
+	$(mChat).on({
+		mchat_add_message_animate_before: function(e, data) {
+			registerDropdown(data.message);
+		},
+		mchat_edit_message_before: function(e, data) {
+			registerDropdown(data.newMessage);
+		}
 	});
 });
